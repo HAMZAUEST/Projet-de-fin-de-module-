58e8c46d8cb4172ee0356b72a9f123c463f784f0
hafdaoui <lilpump0629@gmail.com>
2023-12-31 17:27:32 +0100

    dont know

--- hfbr.py
+++ hfbr.py
@@ -1,3 +1,10 @@
 from secret import password
+
+def my_func(a, b):
+    return a + b
+
 print('hello world')
 print('where do you live')
+
+# Fixing the missing argument for my_func
+print(my_func(3, 4))
